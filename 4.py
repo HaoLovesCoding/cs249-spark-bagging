e1afdf66bc91c508e5d3c3aa433a55559b3e4c74
HaoLovesCoding <nanocmp@gmail.com>
2017-06-15 21:10:40 -0700

    fix bug of sampledFeatureIndex

--- 4.py
+++ 4.py
@@ -10,6 +10,7 @@ from pyspark.mllib.util import MLUtils
 from operator import add
 from pyspark.mllib.linalg import SparseVector
 import random
+import copy
 class BaggingClassifier():
 
 	def __init__(self,
@@ -19,28 +20,28 @@ class BaggingClassifier():
 				 oob_score=False, 
 				 warm_start=False):
 
-		########### parameters ############
 		self.n_estimators = n_estimators
 		self.sample_probability = sample_probability
 		self.features_num = features_num
 		self.oob_score = oob_score
 		self.warm_start = warm_start
-		self.sampledFeatureIndex=None
+		self.sampledFeatureIndex=[[] for i in range(n_estimators)]
 
-	def __randomSelectFeature(self,line):#No sampling on sample
+	def __randomSelectFeature(self,line,iteration_num):#No sampling on sample
 		line_label=line.label
 		features=line.features
-		if not self.sampledFeatureIndex:
-			self.sampledFeatureIndex=self.__reserviorSampling(self.features_num,len(line.features))
+		#is it neccessary?
+		if not self.sampledFeatureIndex[iteration_num]:
+			self.sampledFeatureIndex[iteration_num]=copy.deepcopy(self.__reserviorSampling(self.features_num,len(line.features)))
 		new_features={}
 		count=0
-		for i in self.sampledFeatureIndex:
+		for i in self.sampledFeatureIndex[iteration_num]:
 			if features[i]!=0:
 				new_features[count]=features[i]
 			count+=1
 		if bool(new_features)==False:
-			new_features[len(self.sampledFeatureIndex)-1]=0
-		return LabeledPoint(line_label,SparseVector(len(self.sampledFeatureIndex),new_features))
+			new_features[len(self.sampledFeatureIndex[iteration_num])-1]=0
+		return LabeledPoint(line_label,SparseVector(len(self.sampledFeatureIndex[iteration_num]),new_features))
 
 	def __reserviorSampling(self,k,n):# select k from n, if k is larger than n, all is selected
 		sample=[]
@@ -52,10 +53,10 @@ class BaggingClassifier():
 				sample[replace_index]=i
 		return sample
 
-	def __randomSelect(self,myRDD):
+	def __randomSelect(self,myRDD,n):
 		result=None
 		while True:
-			result=myRDD.map(self.__randomSelectFeature).sample(False,self.sample_probability)
+			result=myRDD.map(lambda x:self.__randomSelectFeature(x,n)).sample(False,self.sample_probability)
 			if result.isEmpty():
 				continue
 			else:
@@ -65,18 +66,21 @@ class BaggingClassifier():
 	def fit(self, data):
 		trainData = []
 		model = []
+		self.sampledFeatureIndex=[[] for i in range(self.n_estimators)]
+		sampeled_list=data.take(1)
+		total_features=len(sampeled_list[0].features)
+		#It will evaluate the sample to be picked up
 		for i in range(self.n_estimators):
-			self.sampledFeatureIndex=None # The random sampling index would be be initialized again in every iterations
-			cdata=self.__randomSelect(data)
-			numClasses = 2
-			categoricalFeaturesInfo = {}
-			numTrees = 3
+			self.sampledFeatureIndex[i]=self.__reserviorSampling(self.features_num,total_features)
+
+		for i in range(self.n_estimators):
+			cdata=self.__randomSelect(data,i)
 			model.append(LinearRegressionWithSGD.train(cdata, iterations=100, step=0.00000001))
 		return model
-
 
 if __name__ == "__main__":
 	sc = SparkContext(appName = 'testML')
 	data = MLUtils.loadLibSVMFile(sc, 'test_original.txt')
-	baggingClass = BaggingClassifier()
-	modelC = baggingClass.fit(data)
+	baggingClass = BaggingClassifier(sample_probability=1)
+	modelC = baggingClass.fit(data,sc)
+	print(baggingClass.sampledFeatureIndex)
